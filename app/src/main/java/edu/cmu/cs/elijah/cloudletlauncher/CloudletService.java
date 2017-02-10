9126a59702eec43323ad88787cfcb7547677604a
Zhuo Chen <zhuoc@cs.cmu.edu>
2017-02-09 20:13:38 -0500

    Dynamically launches VM and get VM address.

--- app/src/main/java/edu/cmu/cs/elijah/cloudletlauncher/CloudletService.java
+++ app/src/main/java/edu/cmu/cs/elijah/cloudletlauncher/CloudletService.java
@@ -5,15 +5,29 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.RemoteCallbackList;
 import android.os.RemoteException;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+import javax.net.ssl.HttpsURLConnection;
 
 import de.blinkt.openvpn.api.IOpenVPNAPIService;
 import de.blinkt.openvpn.api.IOpenVPNStatusCallback;
@@ -23,7 +37,18 @@ import edu.cmu.cs.elijah.cloudletlauncher.api.ICloudletServiceCallback;
 public class CloudletService extends Service {
     private static final String LOG_TAG = "CloudletService";
 
-    private String cloudletIP = "172.17.0.2";
+    // User info
+    private final String userID = "abc";
+    private final String appID = "xyz";
+
+    // Cloudlet info
+    private String cloudletIP = "8.225.186.10";
+    private int cloudletPort = 9127;
+    private String vmIP = "";
+
+    // Message types
+    private static final int MSG_POST_DONE = 0;
+    private static final int MSG_GET_DONE = 1;
 
     // Consts
     private static final int CONST_OPENVPN_PERMISSION = 0;
@@ -69,33 +94,7 @@ public class CloudletService extends Service {
 
     private final ICloudletService.Stub mBinder = new ICloudletService.Stub() {
         public void findCloudlet() {
-            // load client configuration file for OpenVPN
-            String configStr = "";
-            try {
-                InputStream conf = getApplicationContext().getAssets().open("test.conf");
-                InputStreamReader isr = new InputStreamReader(conf);
-                BufferedReader br = new BufferedReader(isr);
-                String line;
-                while(true) {
-                    line = br.readLine();
-                    if(line == null)
-                        break;
-                    configStr += line + "\n";
-                }
-                br.readLine();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error reading .conf file");
-            }
-
-            // Start connection
-            if (mVPNService != null) {
-                try {
-                    //mVPNService.addNewVPNProfile("test_launcher", true, configStr); // true allows user to edit
-                    mVPNService.startVPN(configStr);
-                } catch (RemoteException e) {
-                    Log.e(LOG_TAG, "Error in starting VPN connection");
-                }
-            }
+            new SendPostRequestAsync().execute("http://" + cloudletIP + ":" + cloudletPort);
         };
 
         public void disconnectCloudlet() {
@@ -103,7 +102,7 @@ public class CloudletService extends Service {
                 try {
                     mVPNService.disconnect();
                 } catch (RemoteException e) {
-                    Log.e(LOG_TAG, "Error in disconnecting VPN service");
+                    Log.e(LOG_TAG, "Error in disconnecting VPN service: " + e.getMessage());
                 }
             }
         };
@@ -120,7 +119,147 @@ public class CloudletService extends Service {
     };
 
 
-    /***** Begin handling connection to OpenVPN service ***********************/
+    /***** Begin handling http connections ********************************************************/
+    private String sendPostRequest(String... paras) {
+        try {
+            URL url = new URL(paras[0]); // here is your URL path
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            Uri.Builder builder = new Uri.Builder()
+                    .appendQueryParameter("user_id", userID)
+                    .appendQueryParameter("app_id", appID);
+            String query = builder.build().getEncodedQuery();
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(query);
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String serverResponse = readStream(conn.getInputStream());
+                return serverResponse;
+            }
+        }
+        catch(Exception e){
+            Log.e(LOG_TAG, "Error in sending POST message: " + e.getMessage());
+        }
+        return null;
+    }
+    public class SendPostRequestAsync extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... paras) {
+            return sendPostRequest(paras);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                Message msg = Message.obtain();
+                msg.what = MSG_POST_DONE;
+                mHandler.sendMessage(msg);
+            }
+        }
+    }
+
+
+    private String sendGetRequest(String... paras) {
+        try {
+            URL url = new URL(paras[0]);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                String serverResponse = readStream(conn.getInputStream());
+                return serverResponse;
+            }
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Error in sending GET message: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    public class SendGetRequestAsync extends AsyncTask<String , Void ,String> {
+        @Override
+        protected String doInBackground(String... paras) {
+            return sendGetRequest(paras);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                Message msg = Message.obtain();
+                msg.what = MSG_GET_DONE;
+                mHandler.sendMessage(msg);
+            }
+        }
+    }
+
+    // Converting InputStream to String
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error in reading response from server: " + e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {}
+            }
+        }
+        return response.toString();
+    }
+
+    /***** Begin handling connection to OpenVPN service *******************************************/
+    private void connectVPN() {
+        // load client configuration file for OpenVPN
+        String configStr = "";
+        try {
+            InputStream conf = getApplicationContext().getAssets().open("test.conf");
+            InputStreamReader isr = new InputStreamReader(conf);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while(true) {
+                line = br.readLine();
+                if(line == null)
+                    break;
+                configStr += line + "\n";
+            }
+            br.readLine();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error reading .conf file: " + e.getMessage());
+        }
+
+        // Start connection
+        if (mVPNService != null) {
+            try {
+                mVPNService.startVPN(configStr);
+            } catch (RemoteException e) {
+                Log.e(LOG_TAG, "Error in starting VPN connection: " + e.getMessage());
+            }
+        }
+    }
+
     private IOpenVPNStatusCallback mCallback = new IOpenVPNStatusCallback.Stub() {
         @Override
         public void newStatus(String uuid, String state, String message, String level)
@@ -132,7 +271,7 @@ public class CloudletService extends Service {
                 try {
                     callbackList.getBroadcastItem(i).message(state + "|" + message);
                     if (state.equals("CONNECTED")) {
-                        callbackList.getBroadcastItem(i).newServerIP(cloudletIP);
+                        callbackList.getBroadcastItem(i).newServerIP(vmIP);
                     }
                 } catch (RemoteException e) {
                     // RemoteCallbackList will take care of removing dead objects
@@ -149,7 +288,7 @@ public class CloudletService extends Service {
             try {
                 mVPNService.registerStatusCallback(mCallback);
             } catch (RemoteException e) {
-                Log.e(LOG_TAG, "Error in registering callback to OpenVPN service");
+                Log.e(LOG_TAG, "Error in registering callback to OpenVPN service: " + e.getMessage());
             }
         }
 
@@ -159,6 +298,26 @@ public class CloudletService extends Service {
             mVPNService = null;
         }
     };
-    /***** End handling connection to OpenVPN service *************************/
+    /***** End handling connection to OpenVPN service **********************************************/
 
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            if (msg.what == MSG_POST_DONE) {
+                final Timer pollingTimer = new Timer();
+                TimerTask pollingTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        String response = sendGetRequest("http://" + cloudletIP + ":" + cloudletPort + "?user_id=" + userID + "&app_id=" + appID);
+                        Log.d(LOG_TAG, response);
+                        if (response != null && !response.equals("None")) {
+                            pollingTimer.cancel();
+                            vmIP = response;
+                            connectVPN();
+                        }
+                    }
+                };
+                pollingTimer.schedule(pollingTask, 10000, 3000);
+            }
+        }
+    };
 }
